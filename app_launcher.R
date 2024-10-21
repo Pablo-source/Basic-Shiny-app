3894dd5e1490de6e112aa5d5aac5bc942907cf59
Pablo-source <datavizvalencia@gmail.com>
2024-10-21 09:08:46 +0100

    Added explanation to runApp() function to trigger Shiny app

--- app_launcher.R
+++ app_launcher.R
@@ -1,5 +1,11 @@
 ## R Script: app_launcher.R
 # File: app_launcher.R
 
+# Using runApp() function to trigger our Shiny app
+
+# This script basically triggers the final Shiny app. 
+# Using runApp() function runs a Shiny application, It normally does not return any output.
+
+
 # Use {shiny} runApp() function to launch Shiny-app-using-COVID-data app. 
 runApp('COVID_19_Shiny_app.R')
